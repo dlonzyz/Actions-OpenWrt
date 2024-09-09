e00cb5c554372764be701f7098c82e3f2cf4bdc4
dlonzyz <dlonzyz@gmail.com>
2024-09-09 12:42:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's@https://github.com/openwrt/telephony.git@https://github.com/dlonzyz/telephony.git@g' feeds.conf.default
