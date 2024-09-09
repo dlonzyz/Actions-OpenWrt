7ce37cfc19fad617789d38070c8767a19868e250
dlonzyz <dlonzyz@gmail.com>
2024-09-09 12:43:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i 's@https://github.com/openwrt/telephony.git@https://github.com/dlonzyz/telephony.git@g' feeds.conf.default
